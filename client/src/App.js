430f292c0a3556728db03883cacc987335e3a3f9
Matiaspp96 <matiaas.p@gmail.com>
2022-07-11 00:32:06 -0300

    fix:(landing)

--- client/src/App.js
+++ client/src/App.js
@@ -5,34 +5,32 @@ import Detail from './components/Detail';
 import Home from './components/Home';
 import Landing from './components/Landing';
 import s from './App.css'
-import Loading from './components/Loading';
+// import Loading from './components/Loading';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(()=>{
-      setLoading(false)
-    },2000)
-  })
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false)
+  //   }, 1000)
+  // })
 
   return (
     <div className={s.App}>
-      {loading ? <Loading/> :
       <Switch>
-      <Route exact path='/'>
-        <Landing />
-      </Route>
-      <Route path='/dogs/:id'>
-        <Detail/>
-      </Route>
-      <Route path='/dogs' >
-        <Home />
-      </Route>
-      <Route path='/create' >
-        <Create />
-      </Route>
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+        <Route path='/dogs/:id'>
+          <Detail />
+        </Route>
+        <Route path='/dogs' >
+          <Home />
+        </Route>
+        <Route path='/create' >
+          <Create />
+        </Route>
       </Switch>
-      }
     </div>
   );
 }
